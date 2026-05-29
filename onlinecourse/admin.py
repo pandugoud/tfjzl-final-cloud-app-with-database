ea0fc9fff8d2579622cd6898d04820d7a3e681ad
pandugoud <thumala.pandu@gmail.com>
2026-05-29 03:52:48 -0400

    Added Question, Choice, Submission models and updated admin.py

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,16 +1,35 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+# Import models
+from .models import (
+    Course,
+    Lesson,
+    Instructor,
+    Learner,
+    Question,
+    Choice,
+    Submission
+)
 
-
+# Lesson Inline
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+# Choice Inline (inside Question admin)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
+
+
+# Question Inline (optional use in Course if needed)
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
+
+
+# Course Admin
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
@@ -18,13 +37,22 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['name', 'description']
 
 
+# Question Admin
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
+
+# Lesson Admin
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
-
+# Register models
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
